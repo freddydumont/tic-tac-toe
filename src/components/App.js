36e455889f8a5804dd21f66fe45de1f2a183fa50
freddydumont <frederickmorin18@gmail.com>
2017-06-30 12:26:30 -0400

    Add robot face svg

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import evilBot from '../images/evil-bot.svg';
 import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app">
+        <img className="robot" src={evilBot} alt={"Evil robot face"} />
       </div>
     );
   }
