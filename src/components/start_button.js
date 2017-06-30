48e15bbe1ad6f879c23004ed7e47e54d0f8b3fce
freddydumont <frederickmorin18@gmail.com>
2017-06-30 18:20:33 -0400

    Add hover states in css

--- src/components/start_button.js
+++ src/components/start_button.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
 const StartButton = ({ letter }) => {
+
+  const handleHover = (enter) => {
+    // selector is opposite of letter
+    const selector = letter === 'X' ? 'O' : 'X';
+  }
+
   return (
-    <div className="start-button">
+    <div
+      className={"start-button " + letter}
+      onMouseEnter={handleHover(true)}
+      onMouseLeave={handleHover(false)}>
       <span>{letter}</span>
       <p>{letter === 'X' ? "You Start" : "Robot Starts"}</p>
     </div>
