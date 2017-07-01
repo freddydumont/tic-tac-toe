0c396edb25012aaa05c7d2c363505e5b0febaf27
freddydumont <frederickmorin18@gmail.com>
2017-07-01 11:23:12 -0400

    Implement typed.js in welcome_message

--- src/components/welcome_message.js
+++ src/components/welcome_message.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Typed from 'typed.js';
 
-const WelcomeMessage = (props) => {
-  return (
-    <div className="welcome-message" >
-      <p>Puny Human! Will you dare defy me?</p>
-      <p>I am <em><span className="red">Minimax</span></em>. I <span className="underline">cannot</span> be beaten.</p>
-    </div>
-  );
-};
+class WelcomeMessage extends Component {
+
+  componentDidMount() {
+    const typed = new Typed(".welcome-message", {
+      stringsElement: "#typed-strings",
+      typeSpeed: 40,
+      showCursor: false
+    });
+  }
+
+  render() {
+    return (
+      <div className="welcome-message">
+        <div id="typed-strings">
+          <span id="typed"></span>
+          <p>Puny Human! Will you dare defy me?</p>
+          <p>I am <em><span className="red">Minimax</span></em>. I <span className="underline">cannot</span> be beaten.</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default WelcomeMessage;
