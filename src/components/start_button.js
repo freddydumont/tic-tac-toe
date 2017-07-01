c79985b8f2794500d219d2194ee9b4c2214e50a2
freddydumont <frederickmorin18@gmail.com>
2017-07-01 10:12:12 -0400

    Implement animation under X and O
    Ok on fade out but buggy on fade in, needs to be fixed

--- src/components/start_button.js
+++ src/components/start_button.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import evilBot from '../images/evil-bot.svg';
 
 const StartButton = ({ letter }) => {
 
   const opposite = letter === 'X' ? 'O' : 'X';
 
   const toggleHover = () => {
-    // this.setState({ hover: !this.state.hover });
+    // selected letter turns green
     document.querySelector(`.${letter}`).classList.toggle("green");
+    // opposite letter turns red
     document.querySelector(`.${opposite}`).classList.toggle("red");
+    // opposite paragraph disappears
     document.querySelector(`.${opposite} > p`).classList.toggle("hide");
+    // evilBot appears under opposite paragraph
+    document.querySelector(`.${opposite} > img`).classList.toggle("appear");
+    // paragraph order is second
+    document.querySelector(`.${opposite} > p`).classList.toggle("order");
   }
 
   return (
@@ -18,6 +25,7 @@ const StartButton = ({ letter }) => {
       onMouseLeave={toggleHover}>
       <span>{letter}</span>
       <p>{letter === 'X' ? "You Start" : "Minimax Starts"}</p>
+      <img src={evilBot} alt={"Evil robot face"} height={25} width={25} />
     </div>
   );
 };
