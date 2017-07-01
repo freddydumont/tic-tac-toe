668c2fa04ca3009f08904e44d74d3c84c69c4772
freddydumont <frederickmorin18@gmail.com>
2017-07-01 15:17:18 -0400

    Implement cursor by calling secondLine when first is complete

--- src/components/welcome_message.js
+++ src/components/welcome_message.js
@@ -6,18 +6,20 @@ class WelcomeMessage extends Component {
   componentDidMount() {
     // eslint-disable-next-line
     const first = new Typed(".first", {
-      strings: ["Puny Human! ^250 Will you dare defy me?"],
+      strings: ["Puny Human! ^250 Will you dare defy me?^500"],
       typeSpeed: 50,
-      showCursor: false
-      // onComplete: () => { document.querySelector(".typed-cursor").style.display = "none" }
+      onComplete: () => {
+        callSecond();
+        document.querySelector(".typed-cursor").style.display = "none";
+      }
     });
-    // eslint-disable-next-line
-    const second = new Typed(".second", {
-      strings: ["I am <em><span style='color: #e06c75'>Minimax</span></em>. ^250 I <span style='text-decoration: underline'>cannot</span> be beaten."],
-      typeSpeed: 40,
-      showCursor: false,
-      startDelay: 3000
-    })
+    function callSecond() {
+      // eslint-disable-next-line
+      const second = new Typed(".second", {
+        strings: ["I am <em><span style='color: #e06c75'>Minimax</span></em>. ^250 I <span style='text-decoration: underline'>cannot</span> be beaten."],
+        typeSpeed: 40
+      })
+    }
   }
 
   render() {
