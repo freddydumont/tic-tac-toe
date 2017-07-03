3719e30e97c9d685d413342d9992dbbbff596362
freddydumont <frederickmorin18@gmail.com>
2017-07-02 21:35:15 -0400

    Add route for game.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'normalize.css'
 import App from './components/App';
+import Game from './components/game';
 
 ReactDOM.render(
   <Router>
     <Switch>
+      <Route path="/game" component={Game} />
       <Route path="/" component={App} />
     </Switch>
   </Router>
