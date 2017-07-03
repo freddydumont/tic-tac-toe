e719f11df7751b06e2a084c1e76b9dabdb67331a
freddydumont <frederickmorin18@gmail.com>
2017-07-03 11:33:09 -0400

    Add transition between pages

--- src/components/game.js
+++ src/components/game.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { CSSTransitionGroup } from 'react-transition-group'
 
 const Game = () => (
-  <div>Test</div>
+  <CSSTransitionGroup
+    transitionName="example"
+    transitionAppear={true}
+    transitionAppearTimeout={500}
+    transitionEnter={false}
+    transitionLeave={false}>
+    <div>Test</div>
+  </CSSTransitionGroup>
 );
 
 export default Game;
