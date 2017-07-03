1d3901a04b498f06ba0685cb518c1c1343304940
freddydumont <frederickmorin18@gmail.com>
2017-07-02 21:41:11 -0400

    Add links to /game and remove styles from <a>

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import WelcomeMessage from './welcome_message'
 import StartButton from './start_button'
 import evilBot from '../images/evil-bot.svg';
@@ -12,8 +13,12 @@ class App extends Component {
         <WelcomeMessage />
         <h1>Choose your weapon.</h1>
         <div className="buttons-container">
-          <StartButton letter='X' />
-          <StartButton letter='O' />
+          <Link to="/game">
+            <StartButton letter='X' />
+          </Link>
+          <Link to="/game">
+            <StartButton letter='O' />
+          </Link>
         </div>
       </div>
     );
