4c0a559cb77eea5b5edc77c43260d24016c74946
freddydumont <frederickmorin18@gmail.com>
2017-07-07 19:46:56 -0400

    Fix WelcomeMessage
    * Removed cursor
    * Display a single string containing two paragraphs

--- src/components/welcome_message.js
+++ src/components/welcome_message.js
@@ -4,30 +4,20 @@ import Typed from 'typed.js';
 class WelcomeMessage extends Component {
 
   componentDidMount() {
+    const strings = ["<p>Puny Human! ^250 Will you dare defy me?^500</p>" +
+      "<p>I am <em><span style='color: #e06c75'>Minimax</span></em>. ^250 I <span style='text-decoration: underline'>cannot</span> be defeated.</p>"]
     // eslint-disable-next-line
     const first = new Typed(".first", {
-      strings: ["Puny Human! ^250 Will you dare defy me?^500"],
+      strings,
       typeSpeed: 50,
-      onComplete: () => {
-        callSecond();
-        document.querySelector(".typed-cursor").style.display = "none";
-      }
+      showCursor: false
     });
-    function callSecond() {
-      // eslint-disable-next-line
-      const second = new Typed(".second", {
-        strings: ["I am <em><span style='color: #e06c75'>Minimax</span></em>. ^250 I <span style='text-decoration: underline'>cannot</span> be defeated."],
-        typeSpeed: 40
-        //onComplete, send an action that allows buttons to be rendered
-      })
-    }
   }
 
   render() {
     return (
       <div className="welcome-message">
         <p><span className="first"></span></p>
-        <p><span className="second"></span></p>
       </div>
     );
   }
