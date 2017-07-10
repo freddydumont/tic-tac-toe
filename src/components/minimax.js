89d2dd77e1e5eeda1eae7dbb3d75ae90c9a3bf13
freddydumont <frederickmorin18@gmail.com>
2017-07-10 11:29:35 -0400

    Add typed.js content to minimax dialog

--- src/components/minimax.js
+++ src/components/minimax.js
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Typed from 'typed.js';
 import evilBot from '../images/evil-bot.svg';
 
-const Minimax = () => (
-  <div className="minimax">
-    <img src={evilBot} alt={"Evil robot face"} />
-    <div className="dialog">
-      <p>Your turn, Human.</p>
-    </div>
-  </div>
-);
+class Minimax extends Component {
+  constructor(props) {
+    super(props);
+    this.winStrings = ["Hahaha! I have won!", "You lose again, human.",
+      "I am invincible!!!", "Child's play."];
+
+    this.drawStrings = ["You are lucky, for now.",
+      "One mistake, and you shall fall.", "A temporary relief. Your end is unavoidable."];
+
+    this.startingStrings = ["Tic Tac Toe is my jam.", "Beating humans is my thing.", "Warning! I am good at this."]
+
+    this.errorString = ["I AM ERROR"];
+  }
+
+  componentDidMount() {
+    let arr = [this.startingStrings[Math.floor(Math.random() * 3)]]
+    const typed = new Typed(".message", {
+      strings: arr,
+      typeSpeed: 50,
+      showCursor: false
+    });
+  }
+
+  componentDidUpdate() {
+    // if status is win type win strings
+
+    // if status is draw, type draw strings
+  }
+
+  render() {
+    return (
+      <div className="minimax">
+        <img src={evilBot} alt={"Evil robot face"} />
+        <div className="dialog">
+          <p><span className="message"></span></p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Minimax;
