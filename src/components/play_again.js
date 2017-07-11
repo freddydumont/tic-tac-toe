fbc96480c0e2f747b1d3e77b88fab1572d222204
freddydumont <frederickmorin18@gmail.com>
2017-07-11 10:42:16 -0400

    Add modal fade transition

--- src/components/play_again.js
+++ src/components/play_again.js
@@ -3,18 +3,17 @@ import ReactModal from 'react-modal';
 import { Link } from 'react-router-dom';
 
 const PlayAgain = ({ status, openModal, resetGame, initialGameState }) => {
-  // save status as boolean
-  this.gameStatus = status === 'win' ? true : false;
-
   return (
     <ReactModal
       isOpen={openModal}
       className="content"
       overlayClassName="overlay"
-      contentLabel={"Play Again"}>
-      {/* Display proper title depending on status */}
-      <h1 style={{ color: this.gameStatus ? '#e06c75' : '#98c378' }}>
-        {this.gameStatus ? 'You Lose' : 'Draw'}
+      contentLabel={"Play Again"}
+      closeTimeoutMS={500}>
+      {/* Display proper title depending on status
+        * Make title invisible for transition */}
+      <h1 style={{ color: status === 'win' ? '#e06c75' : status === 'draw' ? '#98c378' : 'rgba(0, 0, 0, 0)' }}>
+        {status === 'win' ? 'You Lose' : 'Draw'}
       </h1>
       <h2>Play Again?</h2>
       <div className="choice">
